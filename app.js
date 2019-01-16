e049a02ea2643e7b8c40a78860db2da1d3fe9dde
Jake Ascher <ascherj@gmail.com>
2019-01-16 18:31:07 -0500

    Make GroceryListItem turn bold on mouse hover

--- app.js
+++ app.js
@@ -9,11 +9,25 @@ var GroceryList = (props) => (
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
   }
 
   render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'none'
+    };
+
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
     );
   }
 }
