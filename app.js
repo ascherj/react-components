98a6f34b302d363890b6123c333c145b8bf23fbb
Jake Ascher <ascherj@gmail.com>
2019-01-16 17:27:01 -0500

    Create a GroceryList that dynamically renders an array of groceryItems

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+    <GroceryListItem item={props.groceryItems[0]} />
+    <GroceryListItem item={props.groceryItems[1]} />
+    <GroceryListItem item={props.groceryItems[2]} />
+  </ul>
+);
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+var Apples = () => (
+  <li>Apples</li>
+);
+
+var Oranges = () => (
+  <li>Oranges</li>
+);
+
+ReactDOM.render(<GroceryList groceryItems={['Apples', 'Oranges', 'Bananas']} />, document.getElementById('app'));
